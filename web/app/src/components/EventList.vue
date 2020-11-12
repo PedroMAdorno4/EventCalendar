7de0ceba6eff7a5b04b83118bd0416ec484b1f89
Pedro Mendes Adorno <pedroadorno99@gmail.com>
2020-11-12 15:12:05 -0300

    Front: Adicionado calendario e criacao de eventos

---
+++ web/app/src/components/EventList.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+import EventCard from '@/components/EventCard.vue';
+
+export default {
+    components: {
+        EventCard,
+    },
+    props: {
+        day: {
+            type: Number,
+            default: 1
+        },
+    },
+    data() {
+        return {
+            dayStart: Number,
+            dayEnd: Number
+        }
+    },
+    computed: {
+        ...mapState(["events", "currentDate"]),
+        // dayEvents() {
+        //     events = [];
+        //     var time = new Date(this.currentDate.getFullYear(), this.currentDate.getMonth(), this.day, 0, 0);
+        //     this.dayStart = time.getTime() / 1000;
+
+        //     time = new Date(this.currentDate.getFullYear(), this.currentDate.getMonth(), this.day, 23, 59);
+        //     this.dayEnd = time.getTime() / 1000;
+
+        //     this.events.forEach(event => {
+        //         if(event["start"] >= this.dayStart && event["start"] <= this.dayEnd) {
+
+        //         }
+        //     });
+        // }
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
